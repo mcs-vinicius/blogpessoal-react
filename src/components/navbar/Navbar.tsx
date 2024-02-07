64ed762d1f906cdfa2cf352dad7348e9e9569fa9
Vinicius Monteiro <moonkraft_dev@outlook.com>
2024-02-06 21:20:43 -0300

    First Commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,48 @@
+﻿import { ReactNode, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerts";
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { usuario, handleLogout } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    function logout() {
+
+        handleLogout()
+        ToastAlerta('O Usuário foi desconectado com sucesso!', "sucesso")
+        navigate('/login')
+    }
+
+    if(usuario.token !== "") {
+        component = (
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            { component }
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/services/Service.ts
@@ -0,0 +1,38 @@
+﻿import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://blogpessoal-neh7.onrender.com'
+    //Inserir o endereço do End-Point
+    //baseURL: 'http://localhost:8080'
+    
+})
+
+// Construtores
+export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) =>{
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
+export const login = async(url: string, dados: Object, setDados: Function) =>{
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+    const resposta =  await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) =>{
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) =>{
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deletar = async(url: string, header: Object) => {
+   await api.delete(url, header)
+}
